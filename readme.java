a7b739ed9fe4e184e34bc0dc13d61caa49744ed5
sword art <59269436+jeese1@users.noreply.github.com>
2019-12-27 15:32:43 +0800

    Update readme.java

--- readme.java
+++ readme.java
@@ -1,14 +1,27 @@
 class readme{
   private int num ;
   private String person ;
-  public void tell(String person,int num){
-  this.person = person;
-  this.num = num;
-  System.out.println("Hello World,"+person+"you are the "+num+"th vister!")
+  public void tell(){
+  System.out.println("Hello World,"+person+".you are the "+num+"th vister!");
  }
+  public void setNum(int num){
+	  this.num = num ;
+  }
+  public int getNum(){
+	  return num;
+  }
+  public void setPerson(String person){
+	  this.person = person ; 
+  }
+  public String getPerson(){
+	  return person;
+  }
 }
 public class letter{
  public static void main(String args[]){
- readme myread = new readme("Arthur",42);
+ readme myread = new readme();
+ myread.setNum(2);
+ myread.setPerson("Arthur");
+ myread.tell();
  }
-} 
+}
